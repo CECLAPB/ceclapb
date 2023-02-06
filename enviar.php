2d9dc43c1e662f9b86e13b407faf58b355fa38ae
Julio César Espinoza <ceclapyb@gmail.com>
2023-02-06 00:11:42 -0300

    actualizacion

--- enviar.php
+++ enviar.php
@@ -4,7 +4,7 @@
     $correo= $_POST["correo"];
     $telefono= $_POST["telefono"];
     $mensaje= $_POST["mensaje"];
-    $contenido= "Nombre: " . $nombre . "\nCorreo: " .$correo . "\nTeléfono: " .$telefono . "\nMensaje: " .$mensaje;
+    $contenido= "Nombre: " .$nombre . "\nCorreo: " .$correo . "\nTeléfono: " .$telefono . "\nMensaje: " .$mensaje;
     mail($destino,"Contacto", $contenido);
     header(Location:gracias.html);
 
